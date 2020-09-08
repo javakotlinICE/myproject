f38ebbb8cb5ea9d4c346b9f9b717836d5ad39d7e
ICE <oooddt147@gmail.com>
2020-09-08 14:58:41 +0800

    Java and Kotlin variable , data type

--- src/main/java/com/ice/Hello.java
+++ src/main/java/com/ice/Hello.java
@@ -5,5 +5,15 @@ public class Hello {
 //        System.out.println("Hello world");
         Person p = new Person();
         p.Hello();
+        int age = 27;
+        Integer age2 = 27;
+        char c = '我';
+        Character c2 = 'A';
+        byte b = 120;
+        float weight = 63.3f;
+        float height = 1.7f;
+        boolean adult = true;
+        boolean enroll = false;
+        String name = "ice";
     }
 }
--- src/main/kotlin/com/ice/Hello.kt
+++ src/main/kotlin/com/ice/Hello.kt
@@ -5,6 +5,11 @@ fun main() {
 //    Human().hello()
     val h = Human()
     h.hello()
+    var age = 19
+    age = 20
+    var weight  = 66.3
+    var name : String
+    name = "ice"
 }
 
 class Human() {
